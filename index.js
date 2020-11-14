329ba6c323e4b62b883928926779a8a9ee3e0f4f
Franklin Oguama <turtle.foguama@gmail.com>
2020-11-14 20:10:20 +0000

    Stuff

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+test = (4+5)*100 + 50 / (2 * 5)
+console.log(test)
 
+function print(text) {
+	console.log(text)
+}
+
+print('test')
+
+(function() {
+    var someThings = ...;
+
+    ...
+
+    module.exports.getSomeThings = function() {
+        return someThings();
+    }
+
+}());
+
+// main.js
+
+var things = require("someThings");
+...
+doSomething(things.getSomeThings());
