1181b4e7f18507ee127b134b8fa343fc69d92939
Franklin Oguama <turtle.foguama@gmail.com>
2020-11-15 16:30:58 +0000

    more mistakes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // var functions = require('./Felix_Stuff/functions');
-
+// const { sqrt } = require('mathjs');
+// var math = require('./mathjs')
 function print(text) {
 	console.log(text)
 }
@@ -13,22 +14,49 @@ Math.factorial = function(num) {
 	return s
 }
 
-mathArray = ['3.5', '+', '5', '^', '2'];
+Math.mod = function (divisor, dividend) {
+	return divisor % dividend;
+}
+	// txt = [cos, sin, tan, abs, h, ceil, floor, exp, pi, e, log, pow, random, round, sqrt];
+
+var pos = {
+	func: ['Math.abs', 'Math.acos', 'Math.acosh', 'Math.asin', 'Math.asinh', 'Math.atan', 'Math.atanh', 'Math.cbrt', 'Math.ceil', 'Math.cos', 'Math.cosh', 'Math.exp', 'Math.expm1', 'Math.floor', 'Math.log', 'Math.log10', 'Math.log2', 'Math.pow', 'Math.random', 'Math.round', 'Math.sign', 'Math.sin', 'Math.sinh', 'Math.sqrt', 'Math.tan', 'Math.tanh', 'Math.conv', 'Math.pi', 'Math.e', 'Math.g']
+}
+
+
+mathArray = ['5', 'mod', '2'];
+mathText = '5 mod 2'
 mathSolve = [];
 
-for (b in mathArray) {
-	c = mathArray[b];
-	if (c == '!') {
-		d = mathSolve.pop();
-		c = `Math.factorial(${d})`
-	} else if (c == '^') {
-		d = mathSolve.pop();
-		e = mathArray[mathArray.indexOf(c)+1];
-		mathArray.splice(mathArray.indexOf(e), 1);
-		console.log(e);
-		c = `Math.pow(${d}, ${e})`
-	}
-	mathSolve.push(c);
-}
-console.log(mathSolve);
-console.log(eval(mathSolve.join('')));
+
+// function txtTolst(txt) {
+// 	txt = txt.replace(/ /g, '');
+// 	txt = txt.split('');
+// 	for (x in txt) {
+
+// 	}
+// }
+
+// for (b in mathArray) {
+// 	c = mathArray[b];
+// 	if (c == '!') {
+// 		d = mathSolve.pop();
+// 		c = `Math.factorial(${d})`
+// 	} else if (c == '^') {
+// 		d = mathSolve.pop();
+// 		e = mathArray[mathArray.indexOf(c)+1];
+// 		mathArray.splice(mathArray.indexOf(e), 1);
+// 		console.log(e);
+// 		c = `Math.pow(${d}, ${e})`
+// 	} else if (c == '%' || c == 'mod') {
+// 		c = '%';
+// 	}
+// 	mathSolve.push(c);
+// }
+// console.log(mathSolve);
+// console.log(eval(mathSolve.join('')));
+
+// console.log(pos.func.length)
+// console.log(math.evaluate('1.2 * (2 + 4.5)'));
+
+// console.log(math.sqrt(-4).toString()) // 2i
