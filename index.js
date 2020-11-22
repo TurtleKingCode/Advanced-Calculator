27cf715b62e1e07d104c81c4a518a00416daa434
Franklin Oguama <turtle.foguama@gmail.com>
2020-11-22 18:15:34 +0000

    I just realized that Algebra JS is not Gunna work for me

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var algebra = require('algebra.js');
 
-/*
-YOU HAVE TO FIX THE FACT THAT WE CAN ONLY HANDLE ONE FACTORIAL
-*/
+var Fraction = algebra.Fraction;
+var Expression = algebra.Expression;
+var Equation = algebra.Equation;
+
+var expr1 = new Expression("x");
+expr1 = expr1.add(2);
+expr1 = expr1.multiply(4);
+
+var expr2 = new Expression("x");
+expr2 = expr2.multiply("y");
+expr2 = expr2.multiply(new Fraction(1, 3));
+expr2 = expr2.add(4);
+
+var expr3 = expr1.multiply(expr2);
+
+console.log("(" + expr1.toString() + ")(" + expr2.toString() + ") = " + expr3.toString());
