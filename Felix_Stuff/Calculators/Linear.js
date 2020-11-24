4f2b5ac837939e473497f712e5e7d48e954dbcd1
Franklin Oguama <turtle.foguama@gmail.com>
2020-11-24 01:30:48 +0000

    I finally fixed the issue with linking both files, and learned my lesson on not using version control

--- Felix_Stuff/Calculators/Linear.js
+++ Felix_Stuff/Calculators/Linear.js
@@ -6,25 +6,31 @@ var Fraction = algebra.Fraction;
 var Expression = algebra.Expression;
 var Equation = algebra.Equation;
 
+function round2(val) {
+	return Math.round(val * 100) / 100; 
+}
+
+function print(val) {
+	console.log(val);
+}
+
 function Linear(linear) {
 	var string = linear.replace(/ /g, '');
-	//If it has an equal sighn, shift everything to the left side of the array
-	var array = string.includes('=') ? 
-	[string.split('=')[0]+'-'+'('+string.split('=')[1]+')', '0'] :
-	[string, '0'];
-
+	var array = string.includes('=') ? [string.split('=')[0]+'-'+'('+string.split('=')[1]+')', '0'] : [string, '0'];
+	console.log(array);
 	var side = {
 		left: algebra.parse(array[0]).simplify(),
 		right: algebra.parse(array[1]).simplify()
 	}
+	console.log(side.left.toString());
 	var equation = new Equation(side.left, side.right);
 	var variables = side.left.terms.map(x => x.variables[0].variable);
 	var coefficients = equation.lhs.terms.map(x => x.coefficients.toString());
-	var A = coefficients[0];
-	var B = coefficients[1];
-	var C = side.left.constants[0] * -1;
-	var x = variables[0];
-	var y = variables[1];
+	var x = variables[0] == 'x' ? variables[0] : variables[1];
+	var y = variables[1] == 'y' ? variables[1] : variables[0];
+	var A = variables[0] == 'x' ? coefficients[0] : coefficients[1];
+	var B = variables[1] == 'y' ? coefficients[1] : coefficients[0];
+	var C = side.left.constants.length == 0 ? '0' : side.left.constants[0] * -1;
 	var m = (A * -1) / B;
 	var b = C/B;
 	var times = A < 0 ? -1 : 1;
@@ -32,32 +38,43 @@ function Linear(linear) {
 	standard.lhs = equation.lhs.add(C).multiply(times);
 	standard.rhs = equation.rhs.add(C).multiply(times);
 	var intercept = 'y = ' + equation.solveFor('y');
-	
+	var xVal = true;
+	if (C == '0') {standard.lhs = standard.lhs.subtract(C);}
 	var form = {
 		standard: standard.toString(),
 		intercept: intercept.toString()
 	}
-	function solve(value, m, b) {
+	function solve(value, m, b, takeX) {
 		var yValue = m*value + b;
-		return 'Your y value is ' + yValue + '\n' + 'The point is (' + value + ', ' + yValue + ')' ;
+		var xValue = (value - b) / m;
+		var vvv = takeX == true ? 'y' : 'x';
+		var point = takeX == true ? `(${value}, ${yValue})` : `(${xValue}, ${value})`
+		var val = takeX == true ? yValue : xValue;
+		return 'Your ' + vvv + ' value is ' + val + '\n' + 'The point is ' + point + '.'
 	}
-	Felix.print();
-	Felix.print('Standard Form: ' + form.standard);
-	Felix.print('Y-intercept From: ' + form.intercept);
-	Felix.print('Slope: ' + m);
-	Felix.print('Y-intercept: ' + `(0, ${b})`);
-	Felix.print();
-	Felix.print('If you want to leave type "done".');
-	Felix.print('Otherwhise, if you want to find the value of y given x, supply me a value for x.');
-	Felix.print();
+	print('');
+	print('Standard Form: ' + form.standard);
+	print('Y-intercept From: ' + form.intercept);
+	print('Slope: ' + round2(m));
+	print('Y-intercept: ' + `(0, ${round2(b)})`);
+	print('X-intercept: ' + `(${round2(-b/m)}, 0)`)
+	print('');
+	print('If you want to leave type "done".');
+	print('Otherwise, if you want to toggle between inputting an x value or a y value, type \'no\'.')
+	print('With your inputed value for x or y, I will give you the corresponding value and point.');
+	print('');
 	function asking(){ 
-		statement = prompt('x = ');
-		if (statement.toLowerCase() == 'done') {
-			endLine();
-			FelixCalculate();
+		var question = xVal == true ? prompt('x = ') : prompt('y = ');
+		if (question.toLowerCase() == 'done') {
+			return;
+		} else if (question.toLowerCase() == 'no') {
+			xVal = !xVal;
+			var vvv = !xVal == true ? 'x' : 'y';
+			print(`I see you want to find the ${vvv} value.`)
+			asking();
 		} else {
-			Felix.print(solve(statement, m, b));
-			Felix.print();
+			print(solve(question, m, b, xVal));
+			print('');
 			asking();
 		}
 	}
--- Felix_Stuff/Felix.js
+++ Felix_Stuff/Felix.js
@@ -1,13 +1,15 @@
 const readline = require('readline-sync');
 const setTrigFuncs = require('./settup');
-var algebra = require('algebra.js');
+const algebra = require('algebra.js');
 prompt = readline.question;
 
-var Fraction = algebra.Fraction;
-var Expression = algebra.Expression;
-var Equation = algebra.Equation;
+const Fraction = algebra.Fraction;
+const Expression = algebra.Expression;
+const Equation = algebra.Equation;
 
-var Linear = require('./Calculators/Linear.js')
+const Scientific = require('./Calculators/Scientific.js')
+const Linear = require('./Calculators/Linear.js');
+
 
 class Bot {
 	constructor(name, description) {
@@ -39,7 +41,7 @@ class Bot {
 		];
 		this.calcName = this.Calculators.map(x => x.name);
 		this.calcFuncs = this.Calculators.map(x => x.func);
-		this.defaultCalculator = this.Calculators[1];
+		this.defaultCalculator = this.Calculators[2];
 		this.chosenCalculator;
 	}
 	intro() {
@@ -321,64 +323,6 @@ function Scientific(science) {
 	Felix.revealSolution(System.solution);
 }
 
-function Linear(linear) {
-	var string = linear.replace(/ /g, '');
-	//If it has an equal sighn, shift everything to the left side of the array
-	var array = string.includes('=') ? 
-	[string.split('=')[0]+'-'+'('+string.split('=')[1]+')', '0'] :
-	[string, '0'];
-
-	var side = {
-		left: algebra.parse(array[0]).simplify(),
-		right: algebra.parse(array[1]).simplify()
-	}
-	var equation = new Equation(side.left, side.right);
-	var variables = side.left.terms.map(x => x.variables[0].variable);
-	var coefficients = equation.lhs.terms.map(x => x.coefficients.toString());
-	var A = coefficients[0];
-	var B = coefficients[1];
-	var C = side.left.constants[0] * -1;
-	var x = variables[0];
-	var y = variables[1];
-	var m = (A * -1) / B;
-	var b = C/B;
-	var times = A < 0 ? -1 : 1;
-	var standard = equation;
-	standard.lhs = equation.lhs.add(C).multiply(times);
-	standard.rhs = equation.rhs.add(C).multiply(times);
-	var intercept = 'y = ' + equation.solveFor('y');
-	
-	var form = {
-		standard: standard.toString(),
-		intercept: intercept.toString()
-	}
-	function solve(value, m, b) {
-		var yValue = m*value + b;
-		return 'Your y value is ' + yValue + '\n' + 'The point is (' + value + ', ' + yValue + ')' ;
-	}
-	Felix.print();
-	Felix.print('Standard Form: ' + form.standard);
-	Felix.print('Y-intercept From: ' + form.intercept);
-	Felix.print('Slope: ' + m);
-	Felix.print('Y-intercept: ' + `(0, ${b})`);
-	Felix.print();
-	Felix.print('If you want to leave type "done".');
-	Felix.print('Otherwhise, if you want to find the value of y given x, supply me a value for x.');
-	Felix.print();
-	function asking(){ 
-		statement = prompt('x = ');
-		if (statement.toLowerCase() == 'done') {
-			endLine();
-			FelixCalculate();
-		} else {
-			Felix.print(solve(statement, m, b));
-			Felix.print();
-			asking();
-		}
-	}
-	asking();
-}
-
 function endLine() {
 	Felix.print('-------------------------------------\n\n')
 }
@@ -401,6 +345,11 @@ function setClss(arrays, array, numClss) {
 	return clss;
 }
 
+module.exports = {
+	Calculate: FelixCalculate,
+	endLine: endLine
+}
+
 // -------------------------
 
 /* Felix.intro();
@@ -416,3 +365,4 @@ Felix.print(
 );
 
 FelixCalculate();
+
